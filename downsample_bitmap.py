008abbe75821aa6708738fd8c1a2600c9d45a4d8
Liam Mulshine <lmulshine@college.harvard.edu>
2019-09-20 21:13:43 -0400

    updated photo 17

--- downsample_bitmap.py
+++ downsample_bitmap.py
@@ -8,17 +8,34 @@ from skimage.transform import resize, rotate
 from PIL import Image
 
 
-image = io.imread("K_L2.jpg", as_grey=True)
+image = io.imread("JPGS/IMG_7853.jpg", as_grey=True)
 image = resize(image, (384, 640), mode='symmetric')
-image = rotate(image, 180)
+image = rotate(image, 0)
 
 
 bitmap = np.array(image) * 255
 print(bitmap)
-bitmap = np.dot((bitmap > 100).astype(float), 255)
+bitmap = np.dot((bitmap > 105).astype(float), 255)
 print(bitmap)
 
 plt.imshow(bitmap, cmap='gray')
 plt.show()
 im=Image.fromarray(bitmap.astype(np.uint8))
-im.save('K_L2.bmp')
+im.save('K_L17.bmp')
+
+
+#nPhoto = 5
+#imageFiles = os.listdir('images')
+#for file in imageFiles[1]:
+#    if file.endswith('.jpg'):
+#        # read, resize and rotate jpg
+#        image = io.imread('images/' + file, as_grey=True)
+#        image = resize(image, (384, 640), mode='symmetric')
+#        image = rotate(image, 180)
+#        bitmap = np.array(image) * 255
+#        bitmap = np.dot((bitmap > 80).astype(float), 255)
+#        im=Image.fromarray(bitmap.astype(np.uint8))
+        
+        #        # save photo
+        #        nPhoto = nPhoto + 1
+        #        im.save('K_L' + str(nPhoto) +  '.bmp')
