274228e284339ef8a6e6f96df1d40533608b6523
Liam Mulshine <ljmulshine@gmail.com>
2019-09-21 18:35:02 -0400

    minor changes to main.py

--- ePaper_Driver/main.py
+++ ePaper_Driver/main.py
@@ -5,18 +5,21 @@ import epd7in5
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
+import random
+import os
 
 try:
     epd = epd7in5.EPD()
     epd.init()
-    epd.Clear(0xFF)
-
     
-    print "read bmp file"
-    Himage = Image.open('/home/pi/SmartPictureFrame/images/K_L1.bmp')
+    # select random image from image directory
+    imageNumber = random.randint(1,len(os.listdir('/home/pi/SmartPictureFrame/images')))
+    imagePath = '/home/pi/SmartPictureFrame/images/K_L' + str(imageNumber) + '.bmp'
+    
+    # display image
+    Himage = Image.open(imagePath)
     epd.display(epd.getbuffer(Himage))
-    time.sleep(2)
-        
+    time.sleep(2)    
     epd.sleep()
         
 except:
