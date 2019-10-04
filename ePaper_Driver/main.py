2d6dfdd277e4f50f5aef6f7e67f32223a8586b32
Liam Mulshine <lmulshine@college.harvard.edu>
2019-10-03 22:33:08 -0400

    Added birthday photo and code

--- ePaper_Driver/main.py
+++ ePaper_Driver/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-import epd7in5
+#import epd7in5
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
@@ -12,17 +12,21 @@ try:
     epd = epd7in5.EPD()
     epd.init()
     
-    # select random image from image directory
-    imageNumber = random.randint(1,len(os.listdir('/home/pi/SmartPictureFrame/images')))
-    imagePath = '/home/pi/SmartPictureFrame/images/K_L' + str(imageNumber) + '.bmp'
-    
+    if(time.strftime("%d %b", time.localtime()) == "07 Oct"):
+        imagePath = '/home/pi/SmartPictureFrame/images/HAPPY_BIRTHDAY.bmp'
+    elif(time.strftime("%d %b", time.localtime()) == "04 Oct"):
+        imagePath = '/home/pi/SmartPictureFrame/images/K_L9.bmp'
+    else:
+        # select random image from image directory
+        imageNumber = random.randint(1,len(os.listdir('/home/pi/SmartPictureFrame/images')))
+        imagePath = '/home/pi/SmartPictureFrame/images/K_L' + str(imageNumber) + '.bmp'
+
     # display image
     Himage = Image.open(imagePath)
     epd.display(epd.getbuffer(Himage))
-    time.sleep(2)    
+    time.sleep(2)
     epd.sleep()
         
 except:
-    print 'traceback.format_exc():\n%s' % traceback.format_exc()
     exit()
 
